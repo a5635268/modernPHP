c72e202db7349b72742829e6e54f81a3d5945d9a
zhouxiaogang <xiaogang.zhou@qq.com>
2019-05-06 11:51:04 +0800

    * [【modernPHP专题(6)】类的延迟绑定（后期绑定）](【modernPHP专题(6)】类的延迟绑定（后期绑定）.md) * [【modernPHP专题(7)】生成器语法](【modernPHP专题(7)】生成器语法.md) * [【modernPHP专题(8)】匿名函数和闭包](【modernPHP专题(8)】匿名函数和闭包.md) * [【modernPHP专题(9)】匿名类](【modernPHP专题(9)】匿名类.md)

--- code/test.php
+++ code/test.php
@@ -1,7 +1,23 @@
 <?php
+$result = 0;
+
+// 在回调生成的时候进行赋值;
+$four = function() use ($result){
+    //0 回调生成的时候赋值，也就是$result = 0;
+    var_dump($result);
+};
+
+// 如果使用引用,就能使use里面的变量完成延迟绑定,也就是在调用的时候再赋值;
+$three = function() use (&$result){
+    //1 在调用的时候再赋值进去，也就是1，注意对象类型也属于引用；
+    var_dump($result);
+};
+
+$result += 1;
 
 
-
+$four(); // outputs int(0)
+$three();    // outputs int(1)
 
 
 
